5132391ebab4dc958efc17cbd52f3421e50b193d
Hardian Jatmiko <miko@labster.com>
2017-01-20 10:33:02 +0800

    Add Player script and Player prefab.

---
+++ RunningGame/Assets/Running/Game/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Running.Game
+{
+	public class Player : MonoBehaviour
+	{
+		private readonly InputManager _inputManager = new InputManager();
+
+		private void Update()
+		{
+			_inputManager.Update();
+
+			switch (_inputManager.GetSwipeDirection())
+			{
+				case InputManager.SwipeDirection.Right:
+					Debug.Log("Move Player to Right!");
+					break;
+				case InputManager.SwipeDirection.Left:
+					Debug.Log("Move Player to Left!");
+					break;
+				case InputManager.SwipeDirection.Top:
+					Debug.Log("Player Jumps!");
+					break;
+				case InputManager.SwipeDirection.Bottom:
+					Debug.Log("Player Slides!");
+					break;
+			}			
+		}
+	}
+}
