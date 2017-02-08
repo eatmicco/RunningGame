f36019f65a1bd25fbc14aa7a4ff61ca21165342d
Hardian Jatmiko <eatmicco@hotmail.com>
2017-02-08 09:55:45 +0800

    Add Falling, moving on top of platform.

--- RunningGame/Assets/Running/Game/Player.cs
+++ RunningGame/Assets/Running/Game/Player.cs
@@ -2,18 +2,36 @@
 
 namespace Running.Game
 {
+	// TODO : Add player sliding
+	// TODO : Add scoring system
+	// TODO : Add basic gui (replacable)
 	[RequireComponent(typeof(BoxCollider), typeof(Rigidbody))]
 	public class Player : MonoBehaviour
 	{
+		private enum VerticalState
+		{
+			Jumping,
+			Falling,
+			None
+		}
+
+		private enum OnTopOf
+		{
+			Platform,
+			Obstacle
+		}
+
 		private readonly InputManager _inputManager = new InputManager();
 		private Vector3 _destination;
 		private float _currentVelocity;
 		private int _currentLane = 1;
 		private bool _onMoving;
-		private bool _onJumping;
+		private VerticalState _verticalState;
 		private bool _freezed;
+		private OnTopOf _onTopOf;
 
 		public event System.Action<Collider> PlayerCollided;
+		public Transform BottomRaySource;
 
 		private void Update()
 		{
@@ -34,14 +52,16 @@ namespace Running.Game
 				Moving();
 			}
 
-			if (!_onJumping)
+			if (_verticalState == VerticalState.None)
 			{
 				SwipeVertical(swipeDirection);
 			}
 			else
 			{
-				Jumping();
+				VerticalMovement();
 			}
+
+			RayBottom();
 		}
 
 		private void OnTriggerEnter(Collider other)
@@ -62,16 +82,15 @@ namespace Running.Game
 					if (_currentLane < Settings.LaneCount - 1)
 					{
 						_onMoving = true;
-						_destination = transform.position;
-						_destination.x += Settings.Instance.LaneWidth;
+						_destination.x = transform.position.x + Settings.Instance.LaneWidth;
+						
 					}
 					break;
 				case InputManager.SwipeDirection.Left:
 					if (_currentLane > 0)
 					{
 						_onMoving = true;
-						_destination = transform.position;
-						_destination.x -= Settings.Instance.LaneWidth;
+						_destination.x = transform.position.x - Settings.Instance.LaneWidth;
 					}
 					break;
 			}
@@ -82,8 +101,7 @@ namespace Running.Game
 			switch (swipeDirection)
 			{
 				case InputManager.SwipeDirection.Top:
-					Debug.Log("Jumping");
-					_onJumping = true;
+					_verticalState = VerticalState.Jumping;
 					_currentVelocity = Settings.Instance.JumpingVelocity;
 					break;
 				case InputManager.SwipeDirection.Bottom:
@@ -110,22 +128,58 @@ namespace Running.Game
 			}
 		}
 
-		private void Jumping()
+		private void VerticalMovement()
 		{
 			var speed = _currentVelocity * Time.deltaTime;
 			var threshold = Settings.Instance.JumpingVelocity * Time.deltaTime;
-			if (transform.position.y >= -threshold && transform.position.y <= threshold && _currentVelocity < 0)
+			if (transform.position.y >= _destination.y - threshold && transform.position.y <= _destination.y + threshold && _currentVelocity < 0)
 			{
 				var pos = transform.position;
-				pos.y = 0;
+				pos.y = _destination.y;
 				transform.position = pos;
-				_onJumping = false;
+				_verticalState = VerticalState.None;
 			}
 			else
 			{
+				if (speed < 0)
+				{
+					_verticalState = VerticalState.Falling;
+				}
 				transform.Translate(Vector3.up * speed, Space.World);
 			}
 			_currentVelocity -= Settings.Instance.Gravity;
+		}
+
+		private void RayBottom()
+		{
+			var ray = new Ray(BottomRaySource.position, Vector3.down);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit))
+			{
+				var platform = hit.transform.gameObject.GetComponentInChildren<Platform>();
+				if (platform != null)
+				{
+					if (_onTopOf == OnTopOf.Obstacle && _verticalState == VerticalState.None)
+					{
+						_verticalState = VerticalState.Falling;
+						_currentVelocity = 0;
+						_destination.y = 0;
+					}
+					_onTopOf = OnTopOf.Platform;
+					return;
+				}
+
+				var element = hit.transform.gameObject.GetComponentInChildren<Element>();
+				if (element != null && element.ElementType == ElementType.Obstacle)
+				{
+					_onTopOf = OnTopOf.Obstacle;
+					if (_verticalState == VerticalState.Jumping | _verticalState == VerticalState.Falling)
+					{
+						_destination.y = hit.point.y;
+					}
+					return;
+				}
+			}
 		}
 	}
 }
