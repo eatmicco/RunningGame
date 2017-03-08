896203c9fc86577501efbd8283293d336dcb1f7e
Hardian Jatmiko <eatmicco@hotmail.com>
2017-03-08 09:44:27 +0800

    Add MenuController.cs.

---
+++ RunningGame/Assets/Running/Menu/MenuController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Running.Menu
+{
+	public class MenuController : MonoBehaviour
+	{
+		public GameObject[] MenuPanels;
+
+		public void OpenPanel(int index)
+		{
+			HideAllMenuPanels();
+			MenuPanels[index].SetActive(true);
+		}
+
+		private void Awake()
+		{
+			InitializeControllers();
+		}
+
+		private void Start()
+		{
+			OpenPanel(GlobalSettings.Instance.MenuIndex);
+		}
+
+		private void InitializeControllers()
+		{
+			foreach (var menuGameObject in MenuPanels)
+			{
+				var childMenuController = menuGameObject.GetComponent<ChildMenuController>();
+				childMenuController.MenuController = this;
+			}
+		}
+
+		private void HideAllMenuPanels()
+		{
+			foreach (var menuPanel in MenuPanels)
+			{
+				menuPanel.SetActive(false);
+			}
+		}
+	}
+}
