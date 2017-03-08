a7b8a7ee2a563b347e6905241eeb722fb0cd85a4
Hardian Jatmiko <eatmicco@hotmail.com>
2017-03-08 09:46:09 +0800

    Add MainMenuController.cs.

---
+++ RunningGame/Assets/Running/Menu/MainMenuController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Running.Menu
+{
+	public class MainMenuController : ChildMenuController
+	{
+		public Button StartButton;
+
+		private void Awake()
+		{
+			if (StartButton != null)
+			{
+				StartButton.onClick.AddListener(HandleStartButtonClicked);
+			}
+		}
+
+		private void HandleStartButtonClicked()
+		{
+			SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+		}
+	}	
+}
