6e57a8f80fc2d548593f30bed98b99564070bd06
Hardian Jatmiko <eatmicco@hotmail.com>
2017-03-08 09:45:41 +0800

    Add GlobalSettings.cs.

---
+++ RunningGame/Assets/Running/GlobalSettings.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace Running
+{
+	public class GlobalSettings : MonoBehaviour
+	{
+		public int MenuIndex;
+		public int CurrentScore;
+
+		private static GlobalSettings _instance;
+
+		public static GlobalSettings Instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					_instance = FindObjectOfType<GlobalSettings>();
+				}
+				return _instance;
+			}
+		}
+
+		public void Awake()
+		{
+			DontDestroyOnLoad(this);
+		}
+
+		public void Start()
+		{
+			SceneManager.LoadScene("MenuScene");
+		}
+	}	
+}
