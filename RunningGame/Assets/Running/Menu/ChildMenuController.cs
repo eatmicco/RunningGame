1cf11f926e60dcf5449c538d5ed80efc77fe7329
Hardian Jatmiko <eatmicco@hotmail.com>
2017-03-08 09:44:50 +0800

    Add ChildMenuController.cs.

---
+++ RunningGame/Assets/Running/Menu/ChildMenuController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+namespace Running.Menu
+{
+	public class ChildMenuController : MonoBehaviour
+	{
+		public MenuController MenuController { get; set; }
+	}	
+}
