c2c5c1d9bc82af2e797e9522f668d22fe0b6d869
Hardian Jatmiko <eatmicco@hotmail.com>
2017-03-08 09:46:44 +0800

    Add EndGameMenuController.cs.

---
+++ RunningGame/Assets/Running/Menu/EndGameMenuController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using Running;
+using Running.Menu;
+using UnityEngine.UI;
+
+namespace Runnning.Menu
+{
+	public class EndGameMenuController : ChildMenuController
+	{
+		public Button HomeButton;
+		public Text ScoreText;
+
+		private void Awake()
+		{
+			if (HomeButton != null)
+			{
+				HomeButton.onClick.AddListener(HandleHomeButtonClicked);
+			}
+
+			if (ScoreText != null)
+			{
+				ScoreText.text = GlobalSettings.Instance.CurrentScore.ToString();
+			}
+		}
+
+		private void HandleHomeButtonClicked()
+		{
+			MenuController.OpenPanel(0);
+		}
+	}	
+}
