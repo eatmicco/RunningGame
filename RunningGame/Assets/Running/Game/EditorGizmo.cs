11088b78e89fca4070503dd31e19595e7da1871b
Hardian Jatmiko <eatmicco@hotmail.com>
2017-03-15 10:10:51 +0800

    Add EditorGizmo.cs.

---
+++ RunningGame/Assets/Running/Game/EditorGizmo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Running.Editor
+{
+	public class EditorGizmo : MonoBehaviour
+	{
+		private void OnDrawGizmos()
+		{
+			// Draw Red Arrow
+			Gizmos.color = Color.red;
+			Gizmos.DrawLine(new Vector3(0, 0, -2), Vector3.zero);
+			Gizmos.DrawLine(new Vector3(0.5f, 0, -1), Vector3.zero);
+			Gizmos.DrawLine(new Vector3(-0.5f, 0, -1), Vector3.zero);
+		}
+	}
+}
